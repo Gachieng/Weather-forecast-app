8150e6e4b1c3a0c1dd91c24cdaa514ed23ee0e63
Gachieng <143952548+Gachieng@users.noreply.github.com>
2024-04-15 17:47:37 +0300

    API integration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temp);
+}
+function searchCity(city) {
+  let apiKey = "a1e340cfbbe150t0d4o3f1d4bdda2ac8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 function submitResults(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", submitResults);
+
+searchCity("New York");
